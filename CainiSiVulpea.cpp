90499eac4c18bb16759cfaf648ded09a77ae02d5
Alintdg <76122532+Alintdg@users.noreply.github.com>
2020-12-19 22:53:54 +0200

    Update CainiSiVulpea.cpp

--- CainiSiVulpea.cpp
+++ CainiSiVulpea.cpp
@@ -15,13 +15,14 @@ typedef struct board
 } grid[8][8];
 
 
-struct position {
+struct position
+{
     int i, j;
 } moveFrom, moveTo;
 
 
 
- void moveFox();
+void moveFox();
 
 void getMoveFromOnClick(int x1, int y1);
 
@@ -56,36 +57,36 @@ void readMatrix (grid board)                //pentru teste ,urmeaza sa fie initi
 {
     for (int i = 0; i < cols; i++)
         for (int j = 0; j < rows; j++)
-                board[i][j].celltype = 0;
-                board[0][3].celltype=2;      //vulpea
-                board[7][0].celltype=1;      //caine din stanga
-                board[7][2].celltype=1;      //cainele al 2-lea
-                board[7][4].celltype=1;      //cainele al 3-lea
-                board[7][6].celltype=1;      //cainele al 4-lea
+            board[i][j].celltype = 0;
+    board[0][3].celltype=2;      //vulpea
+    board[7][0].celltype=1;      //caine din stanga
+    board[7][2].celltype=1;      //cainele al 2-lea
+    board[7][4].celltype=1;      //cainele al 3-lea
+    board[7][6].celltype=1;      //cainele al 4-lea
 }
-void drawFox( grid board  , int i , int j  )
+void drawFox( grid board, int i, int j  )
 {
-            setcolor(RED);
-            circle( board[i][j].x+l/2, board[i][j].y+l/2, l/6 );
-            setfillstyle(  SOLID_FILL,RED  );
-            floodfill( board[i][j].x+l/2,  board[i][j].y+l/2, RED);
+    setcolor(RED);
+    circle( board[i][j].x+l/2, board[i][j].y+l/2, l/6 );
+    setfillstyle(  SOLID_FILL,RED  );
+    floodfill( board[i][j].x+l/2,  board[i][j].y+l/2, RED);
 }
 void drawDog(grid board,int i,int j)
 {
 
-            setcolor(GREEN);
-            circle(board[i][j].x+l/2,board[i][j].y+l/2,l/6);
-            setfillstyle(SOLID_FILL,GREEN);
-            floodfill(board[i][j].x+l/2,board[i][j].y+l/2,GREEN);
+    setcolor(GREEN);
+    circle(board[i][j].x+l/2,board[i][j].y+l/2,l/6);
+    setfillstyle(SOLID_FILL,GREEN);
+    floodfill(board[i][j].x+l/2,board[i][j].y+l/2,GREEN);
 
 }
 void   matrix(grid board )                    //pentru fiecare patrat din tabla ,indicam coordonatele coltului stanga sus
 {
-            l=(float)getmaxx()/cols;                              //latura unui patratel
-                                                                        //coordonatele coltului stanga sus al tablei
-            board[0][0].x=0;
-            board[0][0].y=0;
-                                                                    //coordonatele coltului stanga sus  pentru fiecare patrat
+    l=(float)getmaxx()/cols;                              //latura unui patratel
+    //coordonatele coltului stanga sus al tablei
+    board[0][0].x=0;
+    board[0][0].y=0;
+    //coordonatele coltului stanga sus  pentru fiecare patrat
     int i,j;
     for(i=0; i<8; i++)
         for(j=0; j<8; j++)
@@ -120,7 +121,7 @@ void drawBoard(grid board)
             if(board[i][j].celltype==2)
                 drawFox(board,i,j);
             else if(board[i][j].celltype==1)
-               drawDog(board,i,j);
+                drawDog(board,i,j);
         }
 
     }
@@ -153,15 +154,15 @@ void menu()
     while(1)
     {
         if( ismouseclick(WM_LBUTTONDOWN) )
-       {
+        {
             x=mousex();
             y=mousey();
             clearmouseclick(WM_LBUTTONDOWN);
             if(x >240 && x<467 && y>258 && y<312)
             {
-                 cleardevice();
-                 readMatrix ( board ) ;
-                 drawBoard(  board );
+                cleardevice();
+                readMatrix ( board ) ;
+                drawBoard(  board );
             }
             if(x >240 && x<467 && y>358 && y<412)
                 exit(0);
@@ -172,58 +173,73 @@ void menu()
 
 void moveFox()
 {
-int x ,y;
+    int x,y;
 
 
-while(1)
-{
+    while(1)
+    {
         if( ismouseclick(WM_LBUTTONDOWN) )
-       {
+        {
             x=mousex();
             y=mousey();
             clearmouseclick(WM_LBUTTONDOWN);
             if( y>=0 && y<=100 && x>=300 && x<=400 )
-            do{
-                   x=mousex();
-                   y=mousey();
-                   clearmouseclick(WM_LBUTTONUP);
-                   cout<<x<<" "<<y<<endl;
-               }while(!ismouseclick(WM_LBUTTONUP));
-               if( y>=100 && y<=200 && x>=200 && x<=300 )
-                    drawFox(board, 1 , 2 );
-
-
-       }
-
-}
-}
-
-
-               /* if( moveFrom.i>=0 && moveFrom.i<=100 && moveFrom.j>=300 && moveFrom.j<=400 )
+            {
+                bool gata=0;
+                do
                 {
-                      registermousehandler(WM_LBUTTONUP, getMoveToOnClick);
+                    if(ismouseclick(WM_LBUTTONUP)  )
+                    {
+                        x=mousex();
+                        y=mousey();
+                        clearmouseclick(WM_LBUTTONUP);
 
-                      cout<<moveTo.j<<" "<<moveTo.i;
-
-
-                        //if( moveTo.i>=100 && moveTo.i<=200 && moveTo.j>=200 && moveTo.j<=300)
+                        if( y>=100 && y<=200 && x>=200 && x<=300 )
+                        {
+                            drawFox(board, 1, 2 );
+                            setcolor(WHITE);
+                            setfillstyle(SOLID_FILL , WHITE );
+                            circle(350,50,25);
+                            floodfill(350,50,WHITE);
+                             gata=1;
+                        }
+                    }
 
                 }
+                while(!gata);
+
+            }
+
+        }
+    }
+}
+
+/* if( moveFrom.i>=0 && moveFrom.i<=100 && moveFrom.j>=300 && moveFrom.j<=400 )
+ {
+       registermousehandler(WM_LBUTTONUP, getMoveToOnClick);
+
+       cout<<moveTo.j<<" "<<moveTo.i;
+
+
+         //if( moveTo.i>=100 && moveTo.i<=200 && moveTo.j>=200 && moveTo.j<=300)
+
+ }
 }
 */
 
 
 
-void getMoveFromOnClick(int x1, int y1){
-     moveFrom.j=x1;
+void getMoveFromOnClick(int x1, int y1)
+{
+    moveFrom.j=x1;
     moveFrom.i=y1;
 
 }
 
 
- void getMoveToOnClick(int x1, int y1){
+void getMoveToOnClick(int x1, int y1)
+{
 
-     moveTo.j=x1;
+    moveTo.j=x1;
     moveTo.i=y1;
- }
-
+}
